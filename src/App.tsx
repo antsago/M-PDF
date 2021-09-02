65f4e86a711581445af9bcf7212dc445b81c4c67
Antonio Sanchez Gomez <antonio.sanchez@cyferd.com>
2021-09-02 16:59:26 +0200

    refactor: extracted MainPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useRef, useState } from "react"
-import { PDFDocument } from 'pdf-lib'
+import React from "react"
 import { CssBaseline, ThemeProvider, createTheme } from "@material-ui/core"
-import PDF from './PDF'
-import DragAndDrop from './DragAndDrop'
+import MainPage from './MainPage'
 import TopBar from './TopBar'
 
 const theme = createTheme({
@@ -17,40 +15,12 @@ const theme = createTheme({
   }
 })
 
-const App = () => {
-  const [sourcePdf, setSourcePdf] = useState(null)
-  const [destinationPdf, setDestinationPdf] = useState(null)
-  const pdfLibDestination = useRef(null)
-
-  useEffect(() => { (async () => {
-    pdfLibDestination.current = await PDFDocument.create()
-  })() }, [])
-
-  const insertPage = async () => {
-    const destination = pdfLibDestination.current
-    const source = await PDFDocument.load(sourcePdf)
-    const [existingPage] = await destination.copyPages(source, [0])
-    destination.insertPage(0, existingPage)
-    setDestinationPdf(await destination.saveAsBase64({ dataUri: true }))
-  }
-
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <TopBar />
-      <div>
-        <DragAndDrop onLoad={setSourcePdf} />
-
-        <h1>Original pdf</h1>
-        <PDF file={sourcePdf} />
-
-        <button onClick={insertPage} disabled={!sourcePdf || !pdfLibDestination}>Insert page</button>
-
-        <h1>Destination pdf</h1>
-        <PDF file={destinationPdf} />
-      </div>
-    </ThemeProvider>
-  )
-}
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <TopBar />
+    <MainPage />
+  </ThemeProvider>
+)
 
 export default App
---
+++ src/MainPage.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef, useState } from "react"
+import { PDFDocument } from 'pdf-lib'
+import PDF from './PDF'
+import DragAndDrop from './DragAndDrop'
+
+const MainPage = () => {
+  const [sourcePdf, setSourcePdf] = useState(null)
+  const [destinationPdf, setDestinationPdf] = useState(null)
+  const pdfLibDestination = useRef(null)
+
+  useEffect(() => { (async () => {
+    pdfLibDestination.current = await PDFDocument.create()
+  })() }, [])
+
+  const insertPage = async () => {
+    const destination = pdfLibDestination.current
+    const source = await PDFDocument.load(sourcePdf)
+    const [existingPage] = await destination.copyPages(source, [0])
+    destination.insertPage(0, existingPage)
+    setDestinationPdf(await destination.saveAsBase64({ dataUri: true }))
+  }
+
+  return (
+    <div>
+      <DragAndDrop onLoad={setSourcePdf} />
+
+      <h1>Original pdf</h1>
+      <PDF file={sourcePdf} />
+
+      <button onClick={insertPage} disabled={!sourcePdf || !pdfLibDestination}>Insert page</button>
+
+      <h1>Destination pdf</h1>
+      <PDF file={destinationPdf} />
+    </div>
+  )
+}
+
+export default MainPage
