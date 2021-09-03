6d37e7451374d433ee13d92b96f005900378f105
Antonio Sanchez Gomez <antonio.sanchez@cyferd.com>
2021-09-03 18:45:03 +0200

    feat: destination as collection of source pages

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState, useCallback } from "react"
 import { PDFDocument } from 'pdf-lib'
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
 import { makeStyles, Typography } from "@material-ui/core"
 import { PDFFile, OnInsert } from './Types'
 import SourcePDF from './SourcePDF'
@@ -13,39 +14,55 @@ const useStyles = makeStyles((theme) => ({
   },
   sources: {
     padding: theme.spacing(2),
-    borderRight: `${theme.spacing(1)}px ${theme.palette.primary.main} solid`
+    borderLeft: `${theme.spacing(0.5)}px ${theme.palette.primary.main} solid`,
   }
 }))
 
-const MainPage = () => {
-  const classes = useStyles()
-  
+const useSourceManager = () => {
   const [sources, setSources] = useState<PDFFile[]>([])
-  const [destinationPdf, setDestinationPdf] = useState<PDFFile>(null)
-  const pdfLibDestination = useRef<PDFDocument>(null)
-
-  useEffect(() => { (async () => {
-    pdfLibDestination.current = await PDFDocument.create()
-  })() }, [])
-
-  const insertPage = useCallback<OnInsert>(async (page, sourceFile, e) => {
-    e.stopPropagation()
-
-    const destination = pdfLibDestination.current
-    const source = await PDFDocument.load(sourceFile.content)
-    const [existingPage] = await destination.copyPages(source, [page-1])
-    destination.insertPage(0, existingPage)
-    setDestinationPdf({
-      id: "destination",
-      content: await destination.saveAsBase64({ dataUri: true }),
-      name: "destination",
-    })
-  }, [])
 
   const addSource = useCallback(
     (newSource: PDFFile) => setSources((oldSources) => [newSource, ...oldSources]),
     [],
   )
+  const getSource = useCallback<(string) => PDFFile>(
+    (id) => sources.find(source => source.id === id),
+    [sources],
+  )
+
+  return { sources, addSource, getSource }
+}
+
+type DestinationPage = { sourceId: string, sourcePage: number }
+
+const MainPage = () => {
+  const classes = useStyles()
+
+  const { sources, addSource, getSource } = useSourceManager()
+  const [destination, setDestination] = useState<DestinationPage[]>([])
+  
+  // const [destinationPdf, setDestinationPdf] = useState<PDFFile>(null)
+  // const pdfLibDestination = useRef<PDFDocument>(null)
+
+  // useEffect(() => { (async () => {
+  //   pdfLibDestination.current = await PDFDocument.create()
+  // })() }, [])
+
+  const insertPage = useCallback<OnInsert>(async (page, sourceFile, e) => {
+    e.stopPropagation()
+    setDestination((prevDestination) => [...prevDestination, { sourceId: sources[0].id, sourcePage: 1 }])
+
+    // const destination = pdfLibDestination.current
+    // const source = await PDFDocument.load(sourceFile.content)
+    // const [existingPage] = await destination.copyPages(source, [page-1])
+    // destination.insertPage(0, existingPage)
+    // setDestinationPdf({
+    //   id: "destination",
+    //   content: await destination.saveAsBase64({ dataUri: true }),
+    //   name: "destination",
+    // })
+  }, [sources])
+
 
   return (
     <div className={classes.root}>
@@ -55,9 +72,24 @@ const MainPage = () => {
           <SourcePDF file={source} key={source.id} onInsert={insertPage} />
         ))}
       </DragAndDrop>
-      <div>
-        <h1>Destination pdf</h1>
-        <SourcePDF file={destinationPdf} />
+      <div className={classes.sources}>
+        <Typography>Destination</Typography>
+        {destination?.map((destinationPage, pageIndex) => (
+          <Document
+            key={pageIndex}
+            file={getSource(destinationPage.sourceId).content}
+            onLoadError={(error) => console.log(error)}
+            onLoadSuccess={() => { /* Do nothing */ }}
+          >
+            <Page
+              pageNumber={destinationPage.sourcePage}
+              width={100}
+              renderAnnotationLayer={false}
+              renderInteractiveForms={false}
+              renderTextLayer={false}
+            />
+          </Document>
+        ))}
       </div>
     </div>
   )
