1c3992247fb5551b7165c5e5a6a6b833c86aadcd
Antonio Sanchez Gomez <antonio.sanchez@cyferd.com>
2021-09-12 17:24:49 +0200

    feat: added no pdf messages

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -18,7 +18,11 @@ const useStyles = makeStyles((theme) => ({
   },
   sectionTitle: {
     paddingBottom: theme.spacing(1),
-  }
+  },
+  message: {
+    margin: `0px auto`,
+    padding: theme.spacing(2),
+  },
 }))
 
 const MainPage = () => {
@@ -35,6 +39,11 @@ const MainPage = () => {
         {sources?.map((source) => (
           <SourcePDF file={source} key={source.id} onInsert={insertPage} />
         ))}
+        {!sources?.length && (
+          <Typography component="p" variant="body2" align="center" className={classes.message}>
+            {intl.formatMessage({ defaultMessage: "No sources selected. Drag the pdf files or use the upload button in the top right corner to add new sources." })}
+          </Typography>
+        )}
       </div>
       <div className={classes.section}>
         <Typography color="secondary"  component="h2" variant="h6" className={classes.sectionTitle}>
--- src/components/Destination.tsx
+++ src/components/Destination.tsx
@@ -1,14 +1,23 @@
 import React from "react"
 import { useIntl } from "react-intl"
 import { Document } from 'react-pdf/dist/esm/entry.webpack'
+import { Typography, makeStyles } from "@material-ui/core"
 import { Close as DeleteIcon } from "@material-ui/icons"
 import { usePdfManager } from "../pdfManager"
 import Masonry from "./Masonry"
 import PDFPage from "./PDFPage"
 import PageActionButton from "./PageActionButton"
 
+const useStyles = makeStyles((theme) => ({
+  message: {
+    margin: `0px auto`,
+    padding: theme.spacing(2),
+  }
+}))
+
 const Destination = () => {
   const intl = useIntl()
+  const classes = useStyles()
   const { destination, getSource, deletePage } = usePdfManager()
 
   return (
@@ -26,6 +35,11 @@ const Destination = () => {
           </PDFPage>
         </Document>
       ))}
+      {!destination?.length && (
+        <Typography component="p" variant="body2" align="center" className={classes.message}>
+          {intl.formatMessage({ defaultMessage: "No page added to destination. Use the plus buttons or drag the pages across to add them to the destination pdf." })}
+        </Typography>
+      )}
     </Masonry>
   )
 }
